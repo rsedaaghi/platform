ae636a3e3fb6c50f30993bf3ca4fa639f3ef376b
rsedaaghi <rsedaaghi@icloud.com>
2018-09-06 04:50:30 +0430

    Signup page Getting ready

---
+++ js/myJS.js
@@ -0,0 +1,11 @@
+function myFunc()
+{
+    /* dont' forget to use commands such as these:
+    "string".length
+    
+    confirm("Hey there");
+    var msg = prompt("Hey there");
+    document.getElementById("demo").innerHTML = msg;
+   */
+    document.getElementById("demo").innerHTML = prompt("Hey there");
+}
